4da8ecf8c69b824a8fcf31e35d00c729d8475a14
dhanush <dhanushkumar4211@gmail.com>
2026-05-20 12:31:31 +0530

    fix streamlit path imports

--- app/ui/dashboard.py
+++ app/ui/dashboard.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+
+if str(ROOT_DIR) not in sys.path:
+    sys.path.append(str(ROOT_DIR))
+    
 import time
 import graphviz
 
